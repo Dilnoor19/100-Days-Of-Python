5dbb6e0a68a94ab69bdd636f7b21f8107313585e
Dilnoor singh <dilnoor3179@gmail.com>
2026-06-26 17:16:09 +0530

    Add files via upload

---
+++ random_module.py
@@ -0,0 +1,10 @@
+'''random module - It provide a random item , object or alfabaet or anything you want to choose '''
+import random
+a  =  random.randint(1,5)
+print(a)
+
+b = random.random() * 10
+print(b)
+
+c = random.uniform(1,10)
+print(c)
