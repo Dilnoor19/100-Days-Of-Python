04934f130b39e70941ca64c1b454307674fe4243
Dilnoor singh <dilnoor3179@gmail.com>
2026-06-30 17:27:35 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,14 @@
+# tu = ("HI","by","oom")
+# print(tu[1:2])
+
+# x = ("apple", "banana", "cherry")
+# y = list(x)
+# y.append("kiwi")
+# x = tuple(y)
+# print(x)
+
+# tuplle = ("HI","BY","SHY")
+# y = ("tyy",)
+# tuplle += y
+# print(tuplle)
+
