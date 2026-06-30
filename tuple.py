f219936e5884c3c3f90490fe5be1338bde6dbcba
Dilnoor singh <dilnoor3179@gmail.com>
2026-06-30 17:45:23 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -1,14 +1,27 @@
-# tu = ("HI","by","oom")
-# print(tu[1:2])
+'''Basic level'''
 
-# x = ("apple", "banana", "cherry")
-# y = list(x)
-# y.append("kiwi")
-# x = tuple(y)
-# print(x)
+# Q1 Ek tuple (10, 20, 30, 40, 50) banao aur 3rd element print karo.
+a = (10,20,30,40,50)
+print(a[2])
 
-# tuplle = ("HI","BY","SHY")
-# y = ("tyy",)
-# tuplle += y
-# print(tuplle)
+# Q2 Tuple (1, 2, 3, 4, 5) ka length nikaalo.
+a1 = (1,2,3,4,5)
+print(len(a1))
+
+# Q3 Tuple ("apple", "banana", "mango") me "banana" hai ya nahi check karo.
+A  = ("apple", "banana", "mango")
+if "banana" in A:
+    print(True)
+else:
+    print(False)
+
+# Q4 Tuple (10, 20, 30, 40, 50) ka last element print karo.
+a2 = (10, 20, 30, 40, 50)
+print(a2[-1])
+
+# Q5 Tuple (1, 2, 3) aur (4, 5, 6) ko join karke naya tuple banao.
+A1 = (1,2,3)
+A2 = (4,5,6)
+A3 = A1 + A2
+print(A3)
 
