f9a0afad2b23b2da951cf134290df1f48d4962fa
Dilnoor singh <dilnoor3179@gmail.com>
2026-07-29 06:31:11 +0530

    Uncomment ISS position retrieval code

--- api.py
+++ api.py
@@ -1,14 +1,14 @@
 import requests
 
 
-# response = requests.get("http://api.open-notify.org/iss-now.json")
-# response.raise_for_status()
+response = requests.get("http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
 
-# data = response.json()
+data = response.json()
 
-# longitude = data["iss_position"]["longitude"]
-# latitude = data["iss_position"]["latitude"]
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
 
-# iss_position = (longitude,latitude)
+iss_position = (longitude,latitude)
 
-# print(iss_position)
+print(iss_position)
