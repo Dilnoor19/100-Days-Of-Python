a27be8fb03561b3e452f38633503c028b315fda7
Dilnoor singh <dilnoor3179@gmail.com>
2026-09-21 00:17:22 +0530

    Enhance data types examples in 02_Data_types.py
    Added examples for type identification and conversion.

--- 02_Data_types.py
+++ 02_Data_types.py
@@ -16,3 +16,21 @@ unique_numbers = {1, 2, 3}      # set
 print(10 + 3)   # 13
 print(10 // 3)  # 3 (floor division)
 print(2 ** 4)   # 16 (exponentiation)
+
+# practise 
+# 1> Type Identification
+print(type(42)) #int
+print(type(3.14)) #float
+print(type(True)) #bool
+print(type([1, 2])) #list
+print(type({"name": "Alice"})) #dict
+print(type({1, 2})) #set
+print(type((5,))) #tuple
+
+# 2> Type Conversion
+string_to_int = int("123")
+int_to_float = float(15)
+list_to_tuple = tuple([1, 2, 3])
+set_to_list = list({"a": 1, "b": 2})
+
+print(set_to_list)
