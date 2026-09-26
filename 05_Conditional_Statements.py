cc9d5926c2c73d0c3575adb8fca37bc52dcfb545
Dilnoor singh <dilnoor3179@gmail.com>
2026-09-27 02:36:23 +0530

    Implement basic conditional statements examples
    Added examples of conditional statements, including if, elif, and else structures, as well as truthy and falsy values in Python.

--- 05_Conditional_Statements.py
+++ 05_Conditional_Statements.py
@@ -1,1 +1,42 @@
+# Basic conditional structure
+temperature = 22
 
+if temperature > 30:
+    print("It's hot outside!")
+elif temperature > 20:
+    print("It's a nice day!")
+else:
+    print("It's cold outside!")
+
+
+# Basic if statement
+age = 18
+
+if age >= 18:
+    print("You are an adult.")
+    print("You can vote.")
+
+# If statement with multiple conditions (using logical operators)
+age = 25
+has_license = True
+
+if age >= 18 and has_license:
+    print("You can drive.")
+
+# If statement with a single line of code
+if age > 65: print("Senior citizen discount applied.")
+
+# Falsy values in Python
+if False:     print("This won't print")
+if None:      print("This won't print")
+if 0:         print("This won't print")
+if "":        print("This won't print")
+if []:        print("This won't print")
+if {}:        print("This won't print")
+if set():     print("This won't print")
+
+# Truthy values in Python
+if True:      print("This will print")
+if 42:        print("This will print")
+if "hello":   print("This will print")
+if [1, 2, 3]: print("This will print")
